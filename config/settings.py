10fb0d107527816ac628e50886c27185be0122e1
krimovfirdavs <krimovfirdavs@gmail.com>
2026-06-04 17:21:11 +0500

    fix

--- config/settings.py
+++ config/settings.py
@@ -130,3 +130,10 @@ BOT_TOKEN = "8334723873:AAH73YJHUGDr2GvrKAN-m-qWIw5OeE9aVKY"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://buyukipakyuli.uz",
+    "https://www.buyukipakyuli.uz",
+]
+
