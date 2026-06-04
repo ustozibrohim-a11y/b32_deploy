3bd215ebb2f74f48705f5d3775c6e22e383af6b8
krimovfirdavs <krimovfirdavs@gmail.com>
2026-06-04 16:49:36 +0500

    fix

--- config/settings.py
+++ config/settings.py
@@ -34,12 +34,14 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    "whitenoise.runserver_nostatic",
     'django.contrib.staticfiles',
     "main"
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -77,11 +79,10 @@ DATABASES = {
         "NAME": "mydb",
         "USER": "myuser",
         "PASSWORD": "mypassword",
-        "HOST": "db",        # service nomi
+        "HOST": "db",  # service nomi
         "PORT": "5432",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -116,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@ while ! nc -z db 5432; do
   sleep 1
 done
 
+python manage.py collectstatic
 python manage.py migrate
 
 python manage.py runserver 0.0.0.0:8000
